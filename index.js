1fb1c578f9bb8e8a09d97cc4310ec4de4b4220c2
billycode77 <billycode77@outlook.com>
2020-09-21 19:12:19 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+$("#card-panel-1").click(function(){
+  
+   $(".card-panel-1").hide(slow);
+  
+  
+});
+
+$("#card-panel-2").click(function(){
+  
+  $(".card-panel-2").hide(slow);
+  
+  
+})
+
+$("#card-panel-3").click(function(){
+  
+  $(".card-panel-3").hide(slow);
+  
+  
+})
+
+$("#card-panel-4").click(function(){
+  
+  $(".card-panel-4").hide(slow);
+  
+  
+})
